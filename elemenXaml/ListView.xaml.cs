b28271f8283cca38d29b2a23d04a08016c9dd4d3
Opilane TTHK <opilane@tthk.ee>
2020-10-21 12:28:00 +0300

    first commit

---
+++ elemenXaml/ListView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace elemenXaml
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListView : ContentPage
+    {
+        public string[] Phones { get; set; }
+        public ListView()
+        {
+            InitializeComponent();
+            Phones = new string[] { "iPhone 8", "Samsung Galaxy S9", "Huawei P10", "LG G6" };
+            this.BindingContext = this;
+        }
+    }
+}
---
+++ elemenXaml/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace elemenXaml
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Timer());
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new webview());
+        }
+
+        private async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new tableView());
+        }
+
+        private async void Button_Clicked_3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new datapicker());
+        }
+
+        private async void Button_Clicked_4(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Picker());
+        }
+
+        private async void Button_Clicked_5(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new timerPicer());
+        }
+
+        private async void Button_Clicked_6(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListView());
+        }
+    }
+}
